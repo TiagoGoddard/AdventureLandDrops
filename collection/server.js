81f1fb1a31dc59c0e173a7696491d956da7f7a3b
philippeop <pp.89@live.com>
2016-12-24 22:23:54 +0100

    Added average gold and tracked kills

--- collection/server.js
+++ collection/server.js
@@ -10,7 +10,7 @@ const server = new Hapi.Server({
     debug: { request: ['error'] }
 });
 
-server.connection({ port: 25326, routes: { cors: true }});
+server.connection({ port: 13726, routes: { cors: true }});
 
 const itemTypeByName = {};
 for (let itemType of Object.keys(data.items)) {
