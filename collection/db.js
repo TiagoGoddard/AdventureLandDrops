74126421a124fbe2f5dc7d05014a209e4e62d406
philippeop <pp.89@live.com>
2016-12-28 16:40:00 -0500

    Split gold amounts into individual groups, so we can see the chance of getting 400k vs 50k

--- collection/db.js
+++ collection/db.js
@@ -270,15 +270,17 @@ const getExchangesTable = function() {
     return new Promise((res) => {
         runCommand((cmdRes) => {
             let query = `
-                SELECT exchanges.item, exchanges.result, COUNT(*) AS seen, AVG(amount) AS avg_amount, S.total FROM exchanges
-                LEFT OUTER JOIN (SELECT item, COUNT(*) AS total FROM exchanges GROUP BY item) S
-                ON exchanges.item=S.item
-                WHERE exchanges.result <> 'gold' GROUP BY exchanges.item, exchanges.result
-                UNION ALL
-                SELECT exchanges.item, exchanges.result, COUNT(*) AS seen, AVG(amount) AS avg_amount, S.total FROM exchanges
-                LEFT OUTER JOIN (SELECT item, COUNT(*) AS total FROM exchanges GROUP BY item) S
-                ON exchanges.item=S.item
-                WHERE exchanges.result == 'gold' GROUP BY exchanges.item
+            SELECT exchanges.item, exchanges.result, COUNT(*) AS seen, AVG(amount) AS avg_amount, S.total FROM exchanges
+            LEFT OUTER JOIN (SELECT item, COUNT(*) AS total FROM exchanges GROUP BY item) S
+            ON exchanges.item=S.item
+            WHERE exchanges.result <> 'gold' GROUP BY exchanges.item, exchanges.result
+
+            UNION ALL
+
+            SELECT exchanges.item, exchanges.result, COUNT(*) AS seen, amount, S.total FROM exchanges
+            LEFT OUTER JOIN (SELECT item, COUNT(*) AS total FROM exchanges GROUP BY item) S
+            ON exchanges.item=S.item
+            WHERE exchanges.result == 'gold' GROUP BY exchanges.item, exchanges.amount;
                 `;
 
             db.prepare(query).all((err, rows) => {
