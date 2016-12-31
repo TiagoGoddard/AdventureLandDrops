e04958031f0c1dbe19e6aa624ca1e51e06671924
philippeop <pp.89@live.com>
2016-12-30 20:46:07 -0500

    Fixed price estimate

--- routes.js
+++ routes.js
@@ -91,20 +91,38 @@ const itemHandler = function (request, reply) {
     });
 };
 
-function item_level_cost(item, level) {
+function scroll_cost(item, level) {
     let scroll0_cost = 1000, scroll1_cost = 40000, scroll2_cost = 1600000;
     let item_cost = data.items[item].g ? data.items[item].g : 500000;
     let item_grades = data.items[item].grades;
+    if(level == 1) return item_cost + scroll0_cost;
+    else {
+        if(level >= item_grades[1]) return scroll2_cost;
+        if(level >= item_grades[0]) return scroll1_cost;
+        return scroll0_cost;
+    }
+}
+
+/*function item_level_cost(item, level) {
+    let scroll0_cost = 1000, scroll1_cost = 40000, scroll2_cost = 1600000;
+    let item_cost = data.items[item].g ? data.items[item].g : 500000;
+    let item_grades = data.items[item].grades;
+    if(level == 1) return item_cost + scroll0_cost;
+    else {
+        if(level >= item_grades[1]) return scroll2_cost;
+        if(level >= item_grades[0]) return scroll1_cost;
+        return scroll0_cost;
+    }
     let cost_sc0 = (level > item_grades[0] ? item_grades[0] : level) * scroll0_cost;
     let cost_sc1 = (level > item_grades[0] ? level - item_grades[0] : 0) * scroll1_cost;
     let cost_sc2 = (level > item_grades[1] ? 1 : 0) * scroll2_cost;
     return item_cost + cost_sc0 + cost_sc1 + cost_sc2;
-}
+}*/
 
 const upgradesHandler = function(request, reply) {
     reply.view('upgrades', {
         upgrades: upgradeTable,
-        item_level_cost : item_level_cost,
+        scroll_cost : scroll_cost,
         sprites
     });
 };
