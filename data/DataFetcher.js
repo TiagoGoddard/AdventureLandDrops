ac25b0eb19ad1eeee6b36aeb0433f8e1142fc5e0
NexusNull <p.wellershaus@googlemail.com>
2019-01-08 07:36:14 +0100

    Pushed wip features to testing

---
+++ data/DataFetcher.js
@@ -0,0 +1,158 @@
+var request = require("request-promise-native");
+var vm = require("vm");
+var G = require("./data");
+var C = {};
+var FC = {};
+var FM = {};
+var XYWH = {};
+var T = {};
+
+for (let name in G.sprites) {
+    var g = G.sprites[name];
+    if (g.skip) {
+        continue
+    }
+    var e = 4
+        , b = 3
+        , k = "full";
+    if (in_arr(g.type, ["animation"])) {
+        e = 1,
+            k = g.type
+    }
+    if (in_arr(g.type, ["v_animation", "head", "hair", "hat", "s_wings"])) {
+        b = 1,
+            k = g.type
+    }
+    if (in_arr(g.type, ["wings", "body"])) {
+        k = g.type
+    }
+    if (in_arr(g.type, ["emblem", "gravestone"])) {
+        e = 1,
+            b = 1,
+            k = g.type
+    }
+    var l = g.matrix;
+    C[g.file] = {
+        width: 20,
+        height: 20
+    }
+
+    var c = C[g.file].width / (g.columns * b);
+    var n = C[g.file].height / (g.rows * e);
+    for (var h = 0; h < l.length; h++) {
+        for (var f = 0; f < l[h].length; f++) {
+            if (!l[h][f]) {
+                continue
+            }
+            FC[l[h][f]] = g.file;
+            FM[l[h][f]] = [h, f];
+            XYWH[l[h][f]] = [f * b * c, h * e * n, c, n];
+            T[l[h][f]] = k;
+        }
+    }
+}
+
+getGameData = async function () {
+    var self = this;
+    return new Promise(async function (resolve, reject) {
+        try {
+            let code = await request({
+                url: "https://adventure.land/data.js",
+                headers: {
+                    "x-requested-with": "XMLHttpRequest",
+                    "Accept": "application/json, text/javascript, */*; q=0.01",
+                    "user-agent": "AdventureLandBot: (v1.0.0)",
+                    "cookie": "auth=" + self.sessionCookie,
+                }
+            });
+            let sandbox = {};
+            let context = vm.createContext(sandbox);
+            vm.runInContext(code, context);
+            resolve(sandbox.G)
+        } catch (e) {
+            reject("Could not retrieve game data");
+        }
+    });
+};
+
+setInterval(async function () {
+    await updateGameData()
+}, 1000 * 60 * 60);// 1 hour
+function is_array(e){
+    return Array.isArray(e);
+}
+function in_arr(b, d) {
+    if (is_array(b)) {
+        for (var a = 0; a < b.length; a++) {
+            for (var c in d) {
+                if (b[a] === d[c]) {
+                    return true
+                }
+            }
+        }
+    }
+    for (var c in d) {
+        if (b === d[c]) {
+            return true
+        }
+    }
+    return false
+}
+
+async function updateGameData() {
+    try {
+        G = await getGameData();
+        for (let name in G.sprites) {
+            var g = G.sprites[name];
+            if (g.skip) {
+                continue
+            }
+            var e = 4
+                , b = 3
+                , k = "full";
+            if (in_arr(g.type, ["animation"])) {
+                e = 1,
+                    k = g.type
+            }
+            if (in_arr(g.type, ["v_animation", "head", "hair", "hat", "s_wings"])) {
+                b = 1,
+                    k = g.type
+            }
+            if (in_arr(g.type, ["wings", "body"])) {
+                k = g.type
+            }
+            if (in_arr(g.type, ["emblem", "gravestone"])) {
+                e = 1,
+                    b = 1,
+                    k = g.type
+            }
+            var l = g.matrix;
+                C[g.file] = {
+                    width: 20,
+                    height: 20
+                }
+
+            var c = C[g.file].width / (g.columns * b);
+            var n = C[g.file].height / (g.rows * e);
+            for (var h = 0; h < l.length; h++) {
+                for (var f = 0; f < l[h].length; f++) {
+                    if (!l[h][f]) {
+                        continue
+                    }
+                    FC[l[h][f]] = g.file;
+                    FM[l[h][f]] = [h, f];
+                    XYWH[l[h][f]] = [f * b * c, h * e * n, c, n];
+                    T[l[h][f]] = k;
+                }
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports = {
+    getData: function(){
+        return G;
+    }
+};
--- script_compounder.js
+++ script_compounder.js
@@ -1,149 +1,162 @@
-parent.compoundit = (function() {
-    let COLLECTION_SERVER = 'http://localhost:25326';
-    let COLLECTION_SERVER2 = window.aldc_second_server;
-    let API_KEY = window.aldc_apikey;
+var dropServer = "https://drop.adventurecode.club";
+var apiKey = "YOUR_API_KEY";
+//^^ Adjust this to your own api key
+//If you don't already have on request one from me on discord. @NexusNull#6364
 
-    parent.waiting_for_log = false;
-    var startitem;
 
-    function compound(name, level) {
-        if(!parent) {
-            console.error("Unable to get parent object") ;
+var upgrade_data = [];
+
+var old_compound = compound;
+var inv = {};
+var lockTable = {};
+var inventory = character.items;
+
+var InventoryWatcher = {
+    listener: {
+        changed: [],
+        removed: [],
+        received: [],
+    },
+};
+
+InventoryWatcher.registerListener = function (event, callback) {
+    this.listener[event].push(callback);
+};
+
+InventoryWatcher.removeListener = function (event, callback) {
+    for (let i = 0; i < this.listener[event].length; i++) {
+        if (this.listener[event][i] == callback) {
+            this.listener[event].splice(i, 1);
+        }
+    }
+};
+
+function playerListener(player) {
+    let items = player.items;
+    //Detect changes
+    for (var i = 0; i < items.length; i++) {
+        if (items[i] && inventory[i]) {
+            var keys = Object.keys(items[i]).concat(Object.keys(inventory[i]));
+            var change = false;
+            for (let key of keys) {
+                if (items[i][key] != inventory[i][key]) {
+                    change = true;
+                    break;
+                }
+            }
+            if (change) {
+                for (let callback of InventoryWatcher.listener.changed)
+                    callback(i, inventory[i], items[i]);
+            }
+        } else if (!items[i] && inventory[i]) {
+            //Removed an item
+            for (let callback of InventoryWatcher.listener.removed)
+                callback(i, inventory[i]);
+        } else if (items[i] && !inventory[i]) {
+            //Received an item
+            for (let callback of InventoryWatcher.listener.received)
+                callback(i, items[i]);
+        }
+    }
+    inventory = player.items;
+}
+
+
+inv.compound = async function (item0, item1, item2, scroll_num, offering_num) {
+    return new Promise(function (resolve, reject) {
+        if(!character.items[item0] || !character.items[item1] || !character.items[item2])
             return;
-        }
-        if(parent.waiting_for_log) {
-            console.log("Waiting for log... (parent.waiting_for_log == true)");
-        }
-        if(API_KEY.length < 15) {
-            console.error("Invalid API key") ;
+        if(!G.items[character.items[item0].name].compound)
             return;
+        if(!(character.items[item0].name === character.items[item1].name && character.items[item1].name === character.items[item2].name))
+            return;
+        if(!(character.items[item0].level === character.items[item1].level && character.items[item1].level === character.items[item2].level))
+            return;
+        if(!(character.items[item0] && character.items[item1].name && character.items[item1].name.startsWith("csroll")))
+            return;
+        console.log("passed all tests");
+        /*
+        if (character.items[item_index] && G.items[character.items[item_index].name].upgrade) {
+            if (!lockTable[item_index]) {
+                lockTable[item_index] = true;
+                var iteM = character.items[item_index];
+                var scroll = character.items[scroll_index];
+                var offering = !!character.items[offering_index];
+
+                var listener = {
+                    changeListener: function (itemNum, item) {
+                        if (item_index == itemNum) {
+                            lockTable[item_index] = false;
+                            InventoryWatcher.removeListener("changed", listener.changeListener);
+                            InventoryWatcher.removeListener("removed", listener.removedListener);
+                            upgrade_data.push({
+                                item: iteM,
+                                scroll: scroll,
+                                offering: offering,
+                                success: true,
+                            });
+                        }
+                    },
+                    removedListener: function (itemNum, item) {
+                        if (item_index == itemNum) {
+                            lockTable[item_index] = false;
+                            InventoryWatcher.removeListener("removed", listener.removedListener);
+                            InventoryWatcher.removeListener("changed", listener.changeListener);
+                            upgrade_data.push({
+                                item: iteM,
+                                scroll: scroll,
+                                offering: offering,
+                                success: false,
+                            })
+                        }
+                    }
+                };
+
+
+                InventoryWatcher.registerListener("changed", listener.changeListener);
+                InventoryWatcher.registerListener("removed", listener.removedListener);
+                old_compound(item0, item1, item2, scroll_num, offering_num);
+            } else {
+                reject("Item is locked");
+            }
+        } else {
+            reject("Item is not upgradeable");
         }
-        let [slots,items] = find_all_items_namelevel(name, level);
-        if(slots.length >= 3) {
-            compoundItems(slots);
-        }
+        */
+    });
+};
+parent.inv = inv;
+if (!parent.upgradeData) {
+    parent.upgradeData = {};
+}
+
+
+function new_upgrade(item_index, scroll_index, offering_index) {
+    inv.upgrade(item_index, scroll_index, offering_index);
+}
+
+upgrade = new_upgrade;
+
+setInterval(function () {
+    let request = new XMLHttpRequest();
+    request.open("POST", dropServer + "/upgrade");
+    var data = {
+        apiKey: apiKey,
+        upgrades: upgrade_data,
     }
+    upgrade_data = [];
+    request.send(JSON.stringify(data));
+}, 1000 * 30);
 
-    function compoundItems(slots) {
-        parent.waiting_for_log = true;
-        let item = character.items[slots[0]];
-        startitem = {
-            name : item.name,
-            type : "accss",
-            slot : slots[0],
-            level: item.level,
-        };
-
-        let item_base = G.items[item.name];
-        let scroll_type = item_base.grades[0] <= item.level
-            ? item_base.grades[1] <= item.level
-                ? "cscroll2"
-                : "cscroll1"
-            : "cscroll0";
-
-        let [scroll_slot, scroll] = find_item(i => i.name == scroll_type);
-        if(!scroll) {
-            if(parent.character.gold >= G.items[scroll_type].g && !G.maps[parent.current_map].mount) {
-                parent.buy(scroll_type, 1);
-                setTimeout(function() { compoundItems(slots); }, 500);
-                return;
-            }
-            else {
-                parent.ui_log("Can't buy a compounding scroll.");
-            }
-        }
-
-        parent.socket.on('game_log', success_listener);
-        parent.socket.on('game_error', failure_listener);
-
-        parent.socket.emit("compound", {
-            items: slots,
-            scroll_num: scroll_slot,
-            offering_num: -1,
-            clevel: (startitem.level || 0)
-        });
+parent.socket.on("player", playerListener);
+on_destroy = function () {
+    parent.removeEventListener("player", playerListener);
+    let request = new XMLHttpRequest();
+    request.open("POST", dropServer + "/upgrade", false);
+    var data = {
+        apiKey: apiKey,
+        upgrades: upgrade_data,
     }
-
-    let success_listener = (data) => {
-        if(!parent) return;
-        if (data &&
-            (data.message == 'Item upgrade succeeded' || data.message == 'Item combination succeeded')) {
-            if(startitem) {
-                report_result('success');
-                startitem.level++;
-                console.info(`%cCOMPOUNDER: Upgraded ${startitem.name} to +${startitem.level} successfully!`, 'color: green');
-                startitem = undefined;
-            }
-            else {
-                console.error("Log handler still present but startitem undefined?");
-            }
-            parent.socket.removeListener("game_log", success_listener);
-            parent.socket.removeListener("game_error", failure_listener);
-            parent.waiting_for_log = false;
-        }
-    };
-
-    let failure_listener = (data) => {
-        if (data == 'Item upgrade failed' || data == 'Item combination failed') {
-            if(startitem) {
-                report_result('failed');
-                console.info(`%cCOMPOUNDER: Item upgrade of ${startitem.name} failed going from +${startitem.level} to +${startitem.level + 1}`, 'color: red');
-                startitem = undefined;
-            }
-            else {
-                console.error("Log handler still present but startitem undefined?");
-            }
-            parent.socket.removeListener("game_log", success_listener);
-            parent.socket.removeListener("game_error", failure_listener);
-            parent.waiting_for_log = false;
-        }
-    };
-
-    function report_result(outcome) {
-        let payload = {
-            item: startitem.name,
-            level: startitem.level,
-            success : outcome == 'success' ? 1 : 0,
-            key : API_KEY
-        };
-
-        let data = new FormData();
-        data.append('json', JSON.stringify(payload));
-        let content = {
-            method: 'POST',
-            body: data
-        };
-
-        fetch(`${COLLECTION_SERVER}/compound`, content).catch(() => {});
-
-        if(COLLECTION_SERVER2) {
-            fetch(`${COLLECTION_SERVER2}/compound`, content).catch(() => {});
-        }
-    }
-
-    function find_item(filter) {
-        for(let i = 0; i < character.items.length; i++) {
-            let item = character.items[i];
-            if (item && filter(item))
-                return [i, character.items[i]];
-        }
-        return [];
-    }
-
-    function find_all_items_namelevel(name, level) {
-        console.log(`Finding all ${name}+${level}`);
-        let items = [], item_slots = [];
-        for(let i = 0; i < character.items.length; i++) {// foreach item
-            let item = character.items[i];//get it
-            if (item && item.name == name && item.level == level) {//same name and level?
-                item_slots.push(i);  // push it!
-                items.push(character.items[i]);
-            }
-        }
-
-        return [item_slots, items];
-    }
-
-    parent.ui_success("Use the compound script with parent.compoundit(item_name, level_to_compound)");
-    return compound;
-}());
+    upgrade_data = [];
+    request.send(JSON.stringify(data));
+};
